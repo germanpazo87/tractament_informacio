a1158be9c1d26fd331f4ef66c41d37825dc33eca
germanpazo87 <germanpazoprofe@gmail.com>
2026-02-08 15:21:08 +0100

    Update oracle-core.js

--- assets/js/oracle-core.js
+++ assets/js/oracle-core.js
@@ -1,1 +1,7 @@
+const API_KEY = "REPLACE_ME_WITH_API_KEY";
 
+const SYSTEM_PROMPT = "Ets l'Oracle de la Matriu. Un tutor socràtic d'estadística. Sigues gèlid, curt i cyberpunk. Mai donis el resultat numèric. Si l'usuari s'equivoca, respon amb una pregunta que el faci pensar.";
+
+async function askOracle(userInput, sessionContext) { const url = "" + API_KEY;
+
+}
